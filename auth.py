dd6f68424243637d27136698305f5751d3f23523
MakiniFab <makinifarbean@gmail.com>
2024-03-23 15:45:26 -0700

    m

--- auth.py
+++ auth.py
@@ -33,7 +33,7 @@ def check_if_token_revoked(jwt_header, jwt_payload: dict) -> bool:
     return token is not None
 
 #function to identify current user
-@jwt.user_lookup_loader
+# @jwt.user_lookup_loader
 def user_lookup_callback(jwt_header, jwt_data):
     identity = jwt_data["sub"]
     return User.query.filter_by(id = identity).first()
