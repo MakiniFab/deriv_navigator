2d344fe07529ef219a34505f9ad28983837cdf40
MakiniFab <makinifarbean@gmail.com>
2024-03-23 15:39:22 -0700

    m_jwwt

--- auth.py
+++ auth.py
@@ -26,7 +26,7 @@ def admin_required():
 
 
 #function to revoke token after log out
-@jwt.token_in_blocklist_loader
+# @jwt.token_in_blocklist_loader
 def check_if_token_revoked(jwt_header, jwt_payload: dict) -> bool:
     jti = jwt_payload["jti"]
     token = db.session.query(TokenBlocklist.id).filter_by(jti=jti).scalar()
